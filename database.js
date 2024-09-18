3e1c81fdec06251f6960cb02d18a0681f763a2df
Hassan-1995 <63869278+Hassan-1995@users.noreply.github.com>
2024-09-18 12:12:27 +0500

    Savvy_Sync back end

---
+++ database.js
@@ -0,0 +1,518 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+dotenv.config();
+
+const secretKey = process.env.MY_SECRET_KEY;
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+//  USERS -- done this part
+export async function getSingleUser(user_id) {
+  try {
+    const [result] = await pool.query(
+      `
+          SELECT * 
+          FROM users
+          WHERE user_id = ?
+        `,
+      [user_id]
+    );
+    if (result.length === 0) {
+      //   throw new Error(`Client with user id ${user_id} not found`);
+      console.log(`Client with user id ${user_id} not found`);
+    } else {
+      return result[0];
+    }
+  } catch (error) {
+    console.error("Error geting user :", error);
+    return { error: "Error getting user" };
+    // throw error;
+  }
+}
+export async function authenticateUser(mobile_phone_number) {
+  try {
+    const [result] = await pool.query(
+      `
+            SELECT * 
+            FROM users
+            WHERE mobile_phone_number = ?
+          `,
+      [mobile_phone_number]
+    );
+
+    // If no user is found, return a response indicating failure, but no error is thrown
+    if (result.length === 0) {
+      console.log(`User with Mobile Number ${mobile_phone_number} not found`);
+      return { user: null, token: null, error: `User not found` };
+    }
+
+    const user = result[0];
+
+    const token = jwt.sign(
+      {
+        user_id: user.user_id,
+        user_name: user.user_name,
+        mobile_phone_number: user.mobile_phone_number,
+        created_at: user.created_at,
+      },
+      secretKey,
+      {
+        expiresIn: "1h", // Token expires in 1 hour
+      }
+    );
+
+    return { user, token, error: null }; // Return the user and token if found
+  } catch (error) {
+    console.error("Error authenticating user:", error);
+    // Handle the error without throwing it
+    return { user: null, token: null, error: error.message };
+  }
+}
+export async function createNewUser(user_name, mobile_phone_number) {
+  try {
+    const [existingUser] = await pool.query(
+      `
+          SELECT mobile_phone_number 
+          FROM users 
+          WHERE mobile_phone_number = ?`,
+      [mobile_phone_number]
+    );
+
+    if (existingUser.length > 0) {
+      return { error: "User with this mobile number already exists." };
+    } else {
+      const [result] = await pool.query(
+        `
+            INSERT INTO users (user_name, mobile_phone_number, created_at, updated_at)
+            VALUES (?, ?, NOW(), NOW())
+          `,
+        [user_name, mobile_phone_number]
+      );
+      const newUser = await getSingleUser(result.insertId);
+      return { user: newUser, message: "User created successfully." };
+    }
+  } catch (error) {
+    console.error("Error creating new user: ", error);
+    return { error: "Error creating new user" }; // Return an error response instead of throwing it
+  }
+}
+
+//   LEDGERS
+export async function getAllLedgers() {
+  const [rows] = await pool.query("SELECT * FROM ledgers");
+  return rows;
+}
+export async function getAllLedgersByUserID(user_id) {
+  try {
+    const [result] = await pool.query(
+      `
+        SELECT DISTINCT l.ledger_id, l.* 
+        FROM ledgers l
+        LEFT JOIN ledger_sharing ls ON l.ledger_id = ls.ledger_id
+        WHERE l.user_id = ? OR ls.user_id = ?
+    `,
+      [user_id, user_id]
+    );
+    if (result.length === 0) {
+      //   throw new Error(`Ledger with user id ${user_id} not found`);
+      console.log(`Ledger with user id ${user_id} not found`);
+    } else {
+      return result;
+    }
+  } catch (error) {
+    console.error("Error geting ledger:", error);
+    return { error: "Error getting ledger" };
+    // throw error;
+  }
+}
+export async function createLedgerByUserID(user_id, ledger_name) {
+  try {
+    const [result] = await pool.query(
+      `
+            INSERT INTO ledgers (user_id, ledger_name, created_at, updated_at) 
+            VALUES (?, ?, NOW(), NOW())
+          `,
+      [user_id, ledger_name]
+    );
+    if (result.affectedRows === 0) {
+      //   throw new Error(`Error creating new ledger for user id ${user_id}`);
+      console.log(`Error creating new ledger for user id ${user_id}`);
+      return { error: "Error creating new ledger" };
+    } else {
+      return {
+        ledger_id: result.insertId,
+        message: "Ledger created successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error creating ledger:", error);
+    // throw error;
+    return { error: "Error creating ledger" };
+  }
+}
+export async function updateLedgerByLedgerID(ledger_id, ledger_name) {
+  try {
+    const [result] = await pool.query(
+      `
+        UPDATE ledgers 
+        SET ledger_name = ?, updated_at = NOW()
+        WHERE ledger_id = ?
+        `,
+      [ledger_name, ledger_id]
+    );
+    if (result.affectedRows === 0) {
+      //   throw new Error(`Ledger with id ${ledger_id} not found`);
+      console.log(`Ledger with id ${ledger_id} not found`);
+    } else {
+      return {
+        message: "Ledger updated successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error updating ledger:", error);
+    // throw error;
+    return { error: "Error updating ledger" };
+  }
+}
+export async function deleteLedgerByLedgerID(ledger_id) {
+  try {
+    const [result] = await pool.query(
+      `
+            DELETE FROM ledgers 
+            WHERE ledger_id = ?
+          `,
+      [ledger_id]
+    );
+    if (result.affectedRows === 0) {
+      //   throw new Error(`Ledger with id ${ledger_id} not found`);
+      console.log(`Ledger with id ${ledger_id} not found`);
+    } else {
+      return {
+        message: "Ledger deleted successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error deleting ledger:", error);
+    return { error: "Error deleting ledger" };
+    // throw error;
+  }
+}
+
+// PARTICULARS
+export async function getAllParticularsByLedgerID(ledger_id) {
+  try {
+    const [result] = await pool.query(
+      `
+          SELECT *
+          FROM particulars
+          WHERE ledger_id = ?
+        `,
+      [ledger_id]
+    );
+    // Return the result, even if it's an empty array, and handle it in the frontend
+    return result;
+  } catch (error) {
+    console.error("Error getting particular:", error);
+    // throw error; // Keep throwing the error if something went wrong with the query
+    return { error: "Error getting particular" };
+  }
+}
+export async function createParticularByLedgerID(ledger_id, particular_name) {
+  try {
+    const [result] = await pool.query(
+      `
+            INSERT INTO particulars (ledger_id, particular_name, created_at, updated_at) 
+            VALUES (?, ?, NOW(), NOW())
+        `,
+      [ledger_id, particular_name]
+    );
+    if (result.affectedRows === 0) {
+      console.log(`Error creating new particular for ledger id ${ledger_id}`);
+      //   throw new Error(
+      //     `Error creating new particular for ledger id ${ledger_id}`
+      //   );
+    } else {
+      return {
+        particular_id: result.insertId,
+        message: "Particular created successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error creating particular:", error);
+    // throw error;
+    return { error: "Error creating particular." };
+  }
+}
+export async function updateParticularByParticularID(
+  particular_id,
+  particular_name
+) {
+  try {
+    const [result] = await pool.query(
+      `
+            UPDATE particulars 
+            SET particular_name = ?, updated_at = NOW()
+            WHERE particular_id = ?
+          `,
+      [particular_name, particular_id]
+    );
+    if (result.affectedRows === 0) {
+      //   throw new Error(`Particular with id ${particular_id} not found`);
+      console.log(`Particular with id ${particular_id} not found`);
+    } else {
+      return {
+        message: "Particular updated successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error updating particular:", error);
+    // throw error;
+    return { error: "Error updating particular" };
+  }
+}
+export async function deleteParticularByParticularID(particular_id) {
+  try {
+    const [result] = await pool.query(
+      `
+        DELETE FROM particulars 
+        WHERE particular_id = ?
+      `,
+      [particular_id]
+    );
+    if (result.affectedRows === 0) {
+      //   throw new Error(`Particular with id ${particular_id} not found`);
+      console.log(`Particular with id ${particular_id} not found`);
+    } else {
+      return {
+        message: "Particular deleted successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error deleting particular:", error);
+    // throw error;
+    return { error: "Error deleting particular." };
+  }
+}
+
+// ENTRIES
+export async function getAllEntriesByParticularID(particular_id) {
+  try {
+    const [result] = await pool.query(
+      `
+        SELECT *
+        FROM entries
+        WHERE particular_id = ?  
+      `,
+      [particular_id]
+    );
+    return result;
+  } catch (error) {
+    console.error("Error geting entry:", error);
+    // throw error;
+    return { error: "Error getting entry." };
+  }
+}
+export async function createEntryByParticularID(
+  particular_id,
+  amount,
+  date,
+  description,
+  type
+) {
+  try {
+    const [result] = await pool.query(
+      `
+            INSERT INTO entries (particular_id, amount, date, description, type, created_at, updated_at) 
+            VALUES (?, ?, ?, ?, ?, NOW(), NOW())
+          `,
+      [particular_id, amount, date, description, type]
+    );
+    if (result.affectedRows === 0) {
+      //   throw new Error(
+      //     `Error creating new entry for particular id ${particular_id}`
+      //   );
+      console.log(
+        `Error creating new entry for particular id ${particular_id}`
+      );
+    } else {
+      return {
+        entry_id: result.insertId,
+        message: "Entry created successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error creating entry:", error);
+    // throw error;
+    return { error: "Error creating entry" };
+  }
+}
+export async function updateEntryByEntryID(
+  entry_id,
+  amount,
+  date,
+  description,
+  type
+) {
+  try {
+    const [result] = await pool.query(
+      `
+            UPDATE entries 
+            SET amount = ?, date = ?, description = ?, type = ?, updated_at = NOW()
+            WHERE entry_id = ?
+          `,
+      [amount, date, description, type, entry_id]
+    );
+    if (result.affectedRows === 0) {
+      //   throw new Error(`Entry with id ${entry_id} not found`);
+      console.log(`Entry with id ${entry_id} not found`);
+    } else {
+      return {
+        message: "Entry updated successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error updating entry:", error);
+    // throw error;
+    return { error: "Error updating entry." };
+  }
+}
+export async function deleteEntryByEntryID(entry_id) {
+  try {
+    const [result] = await pool.query(
+      `
+        DELETE FROM entries 
+        WHERE entry_id = ?
+        `,
+      [entry_id]
+    );
+    if (result.affectedRows === 0) {
+      //   throw new Error(`Entry with id ${entry_id} not found`);
+      console.log(`Entry with id ${entry_id} not found`);
+    } else {
+      return {
+        message: "Entry deleted successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error deleting entry:", error);
+    // throw error;
+    return { error: "Error deleting entry." };
+  }
+}
+
+// EXTRA FUNCTIONS
+// Calculate the sum in one specific ledger
+export async function getSumAmountFromSpecificLedger(ledger_id) {
+  try {
+    const [result] = await pool.query(
+      `
+        SELECT *
+        FROM entries
+        WHERE particular_id IN (
+                SELECT particular_id
+                FROM particulars
+                WHERE ledger_id = ?
+            )  
+        `,
+      [ledger_id]
+    );
+    return result;
+  } catch (error) {
+    console.error("Error geting sum amount from specific ledger:", error);
+    // throw error;
+    return { error: "Error getting sum amount from specific ledger." };
+  }
+}
+// Calculate the sum in one specific particular
+export async function getSumAmountFromSpecificParticular(particular_id) {
+  try {
+    const [result] = await pool.query(
+      `
+            SELECT *
+            FROM entries
+            WHERE particular_id = ?  
+        `,
+      [particular_id]
+    );
+    return result;
+  } catch (error) {
+    console.error("Error geting sum amount from specific particular: ", error);
+    // throw error;
+    return { error: "Error getting sum amount from specific particular." };
+  }
+}
+// Get data from different coloumns to create PDF/AccountBook file to share
+export async function createPDFSpecificLedger(ledger_id) {
+  try {
+    const [result] = await pool.query(
+      `
+        SELECT particular_name, description, date, amount, type
+        FROM entries 
+        JOIN particulars ON entries.particular_id = particulars.particular_id
+        WHERE particulars.ledger_id = ?
+        ORDER BY date ASC;
+        `,
+      [ledger_id]
+    );
+    return result;
+  } catch (error) {
+    console.error(
+      "Error geting data from different coloumns to create PDF/AccountBook :",
+      error
+    );
+    // throw error;
+    return {
+      error:
+        "Error geeting data from different columns to create PDF/AccountBook",
+    };
+  }
+}
+// Get Access_key for particular ledger
+export async function getAccessKeyForLedger(user_id, ledger_id) {
+  try {
+    const [result] = await pool.query(
+      `
+        SELECT *
+        FROM access_key
+        WHERE user_id = ? AND ledger_id = ?;
+        `,
+      [user_id, ledger_id]
+    );
+    return result;
+  } catch (error) {
+    console.error("Error geting access key for the ledger :", error);
+    // throw error;
+    return { error: "Error geting access key for the ledger" };
+  }
+}
+// Get ledger with access key
+export async function shareCopyOfLedgerWithAccessKey(user_id, access_key) {
+  try {
+    const [result] = await pool.query(
+      `
+        INSERT INTO ledger_sharing (ledger_id, user_id)
+        SELECT ledger_id, ?
+        FROM ledgers
+        WHERE access_key = ?;
+        `,
+      [user_id, access_key]
+    );
+    return result;
+  } catch (error) {
+    console.error("Error creating duplicate of ledger for user id ", error);
+    // throw error;
+    return { error: "Error creating duplicate of ledger for user id" };
+  }
+}
+
+//   Testing Commands
+// const clients = await createCopyOfLedgerWithAccessKey(2, "a224beb824");
+// console.log("clients", clients);
---
+++ server.js
@@ -0,0 +1,289 @@
+import express from "express";
+import {
+  authenticateUser,
+  shareCopyOfLedgerWithAccessKey,
+  createEntryByParticularID,
+  createLedgerByUserID,
+  createNewUser,
+  createParticularByLedgerID,
+  createPDFSpecificLedger,
+  deleteEntryByEntryID,
+  deleteLedgerByLedgerID,
+  deleteParticularByParticularID,
+  getAccessKeyForLedger,
+  getAllEntriesByParticularID,
+  getAllLedgersByUserID,
+  getAllParticularsByLedgerID,
+  getSumAmountFromSpecificLedger,
+  getSumAmountFromSpecificParticular,
+  updateEntryByEntryID,
+  updateLedgerByLedgerID,
+  updateParticularByParticularID,
+} from "./database.js";
+
+const app = express();
+app.use(express.json());
+
+// USERS
+app.post("/login", async (req, res) => {
+  const { mobile_phone_number } = req.body;
+  try {
+    const { user, token } = await authenticateUser(mobile_phone_number);
+    res.status(200).send({ user, token });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+app.post("/register", async (req, res) => {
+  const { user_name, mobile_phone_number } = req.body;
+
+  console.log("Incoming request: ", req.body);
+
+  if (!user_name || !mobile_phone_number) {
+    return res
+      .status(400)
+      .json({ error: "User Name and Mobile Number is required" });
+  }
+
+  try {
+    const result = await createNewUser(user_name, mobile_phone_number);
+
+    if (result.error) {
+      return res.status(400).json({ error: result.error });
+    }
+
+    res.status(201).json(result);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Failed to create user" });
+  }
+});
+
+// LEDGERS
+app.get("/ledgers/:user_id", async (req, res) => {
+  const user_id = req.params.user_id;
+  const clients = await getAllLedgersByUserID(user_id);
+  res.send(clients);
+});
+app.post("/ledgers", async (req, res) => {
+  const { user_id, ledger_name } = req.body;
+
+  console.log("Incoming request: ", req.body);
+  if (!user_id || !ledger_name) {
+    return res
+      .status(400)
+      .json({ error: "User ID and Ledger Name are required" });
+  }
+  try {
+    const result = await createLedgerByUserID(user_id, ledger_name);
+    res.status(201).json(result);
+  } catch (error) {
+    console.error("Error creating ledger:", error);
+    res.status(500).json({ error: "Failed to create ledger" });
+  }
+});
+app.put("/ledgers/:ledger_id", async (req, res) => {
+  const ledger_id = req.params.ledger_id;
+  const { ledger_name } = req.body;
+  try {
+    const result = await updateLedgerByLedgerID(ledger_id, ledger_name);
+    res.send(result);
+  } catch (error) {
+    console.error("Error updating ledger:", error);
+    res.status(500).send({ error: "Failed to update ledger" });
+  }
+});
+app.delete("/ledgers/:ledger_id", async (req, res) => {
+  const ledger_id = req.params.ledger_id;
+  try {
+    const result = await deleteLedgerByLedgerID(ledger_id);
+    res.send(result);
+  } catch (error) {
+    console.error("Error deleting ledger:", error);
+    res.status(500).send({ error: "Failed to delete ledger" });
+  }
+});
+
+// PARTICULARS
+app.get("/particulars/:ledger_id", async (req, res) => {
+  const ledger_id = req.params.ledger_id;
+  const clients = await getAllParticularsByLedgerID(ledger_id);
+  res.send(clients);
+});
+app.post("/particulars", async (req, res) => {
+  const { ledger_id, particular_name } = req.body;
+
+  console.log("Incoming request: ", req.body);
+  try {
+    const result = await createParticularByLedgerID(ledger_id, particular_name);
+    res.status(201).send(result); // Status 201 for resource creation success
+  } catch (error) {
+    console.error("Error creating particular:", error);
+    res.status(500).send({ error: "Failed to create particular" });
+  }
+});
+app.put("/particulars/:particular_id", async (req, res) => {
+  const particular_id = req.params.particular_id;
+  const { particular_name } = req.body;
+  try {
+    const result = await updateParticularByParticularID(
+      particular_id,
+      particular_name
+    );
+    res.send(result);
+  } catch (error) {
+    console.error("Error updating particular:", error);
+    res.status(500).send({ error: "Failed to update particular" });
+  }
+});
+app.delete("/particulars/:particular_id", async (req, res) => {
+  const particular_id = req.params.particular_id;
+  try {
+    const result = await deleteParticularByParticularID(particular_id);
+    res.send(result);
+  } catch (error) {
+    console.error("Error deleting particular:", error);
+    res.status(500).send({ error: "Failed to delete particular" });
+  }
+});
+
+// ENTRIES
+app.get("/entries/:particular_id", async (req, res) => {
+  const particular_id = req.params.particular_id;
+  try {
+    const entries = await getAllEntriesByParticularID(particular_id);
+    res.send(entries);
+  } catch (error) {
+    console.error("Error fetching entries:", error);
+    res.status(500).send({ error: "Failed to fetch entries" });
+  }
+});
+app.post("/entries", async (req, res) => {
+  const { particular_id, amount, date, description, type } = req.body;
+  try {
+    const result = await createEntryByParticularID(
+      particular_id,
+      amount,
+      date,
+      description,
+      type
+    );
+    res.status(201).send(result); // Status 201 for resource creation success
+  } catch (error) {
+    console.error("Error creating entry:", error);
+    res.status(500).send({ error: "Failed to create entry" });
+  }
+});
+app.put("/entries/:entry_id", async (req, res) => {
+  const entry_id = req.params.entry_id;
+  const { amount, date, description, type } = req.body;
+
+  try {
+    const result = await updateEntryByEntryID(
+      entry_id,
+      amount,
+      date,
+      description,
+      type
+    );
+    res.send(result);
+  } catch (error) {
+    console.error("Error updating entry:", error);
+    res.status(500).send({ error: "Failed to update entry" });
+  }
+});
+app.delete("/entries/:entry_id", async (req, res) => {
+  const entry_id = req.params.entry_id;
+  try {
+    const result = await deleteEntryByEntryID(entry_id);
+    res.send(result);
+  } catch (error) {
+    console.error("Error deleting entry:", error);
+    res.status(500).send({ error: "Failed to delete entry" });
+  }
+});
+
+// EXTRA FUNCTIONS
+// Calculate the sum in one specific ledger
+app.get("/ledgerSum/:ledger_id", async (req, res) => {
+  const ledger_id = req.params.ledger_id;
+  try {
+    const ledgerSum = await getSumAmountFromSpecificLedger(ledger_id);
+    res.send(ledgerSum);
+  } catch (error) {
+    console.error("Error fetching the sum in one specific ledger:", error);
+    res.status(500).send({ error: "Failed to fetch ledger sum." });
+  }
+});
+// Calculate the sum in one specific particular
+app.get("/particularSum/:particular_id", async (req, res) => {
+  const particular_id = req.params.particular_id;
+  try {
+    const particularSum = await getSumAmountFromSpecificParticular(
+      particular_id
+    );
+    res.send(particularSum);
+  } catch (error) {
+    console.error("Error fetching the sum in one specific particular:", error);
+    res.status(500).send({ error: "Failed to fetch particular sum." });
+  }
+});
+app.get("/createPDF/:ledger_id", async (req, res) => {
+  const ledger_id = req.params.ledger_id;
+  try {
+    const PDF = await createPDFSpecificLedger(ledger_id);
+    res.send(PDF);
+  } catch (error) {
+    console.error(
+      "Error fetching data from different coloumns to create PDF/AccountBook :",
+      error
+    );
+    res.status(500).send({
+      error:
+        "Failed to fetch data from different coloumns to create PDF/AccountBook.",
+    });
+  }
+});
+app.get("/accessKey/:user_id/:ledger_id", async (req, res) => {
+  const ledger_id = req.params.ledger_id;
+  const user_id = req.params.user_id;
+  try {
+    const accessKey = await getAccessKeyForLedger(user_id, ledger_id);
+    res.send(accessKey);
+  } catch (error) {
+    console.error("Error fetching access key to view ledger :", error);
+    res.status(500).send({
+      error: "Failed to fetch access key to view ledger.",
+    });
+  }
+});
+app.post("/shareLedger", async (req, res) => {
+  const { user_id, access_key } = req.body;
+  // Check if both user_id and access_key are provided
+  if (!user_id || !access_key) {
+    return res
+      .status(400)
+      .json({ error: "user_id and access_key are required" });
+  }
+  try {
+    // Call the function to create a copy of the ledger
+    const result = await shareCopyOfLedgerWithAccessKey(user_id, access_key);
+    if (result.error) {
+      return res.status(500).json({ error: result.error });
+    }
+    res.status(201).json({ message: "Ledger copied successfully", result });
+  } catch (error) {
+    console.error("Error copying ledger:", error);
+    res.status(500).json({ error: "Failed to copy ledger" });
+  }
+});
+
+// mandatory part
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
